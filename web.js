b7b25cfb5b020d81479591225003f68a72c0a0b4
Carl Hinkle <carl@pleasefindmyanimal.com>
2013-07-04 19:54:05 +0000

    Updating web.js to serve index.html

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var buffer = fs.readFileSync('index.html');
+    response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
