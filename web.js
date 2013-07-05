e672c98a57f777153d3009b4a178e7a6c9bfabaf
Craig Newton <newtondev@gmail.com>
2013-07-05 11:55:42 +0000

    Added basic scaffholding layout for bitstarter application.

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
-var express = require('express');
+var express = require('express')
+  , fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var data = fs.readFileSync('./index.html').toString('utf8');
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
