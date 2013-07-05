67cc9536d72e35a750e108c04fcede3bc1606756
Don Kendrick <me@donkendrick.org>
2013-07-05 12:36:16 +0000

    Updated web.js to point to index.html

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-var buf = New Buffer(fs.readFileSync(index.html));
+var buf = New Buffer(fs.readFileSync(./index.html));
 
 app.get('/', function(request, response) {
   response.send(buf.tostring());
