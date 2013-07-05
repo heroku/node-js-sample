2a9551368556a58299bde0bfcf866323f07aee42
Don Kendrick <me@donkendrick.org>
2013-07-05 13:43:12 +0000

    Updated web.js to fix errors

--- web.js
+++ web.js
@@ -1,7 +1,8 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
-var buf = new Buffer(fs.readFileSync('./index.html'),'utf-8');
+var buf = fs.readFileSync('./index.html','utf-8');
 
 app.get('/', function(request, response) {
   response.send(buf.toString());
