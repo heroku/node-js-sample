fd542b46aa4d0bb942de993f4eaf8f23fe4adeae
Daniel <danielpwa@gmail.com>
2013-07-06 20:16:39 +0200

    read file sync

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(fs.readFileSync("index.html").toString());
 });
 
 var port = process.env.PORT || 5000;
