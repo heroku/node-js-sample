917c98577e2637387c515b3cdbbcca1700632ffd
Mohan Ponnada <kuldeep@unixell.in>
2013-07-06 18:20:51 +0000

    Added readFileSyc & buffer read to read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var fs = require('fs')
 
 var app = express.createServer(express.logger());
+var line = fs.readFileSync('index.html','utf8',function(err,data){
+    if(err) throw err;
+    console.log(data);
+});
+
+var linebuf = new Buffer(line,'utf8');
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//  response.send('Hello World 2!');
+    response.send(linebuf.toString());
 });
 
 var port = process.env.PORT || 5000;
