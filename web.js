0ce9ac93c1b52eec4bbe88c171e979009cb0f995
Sangam <sangamstart@gmail.com>
2013-07-06 16:37:59 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
-
+var fs = require('fs');
+var buf = new Buffer(fs.readFileSync("index.html"));
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
