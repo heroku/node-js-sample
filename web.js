7fbb5acaf3793470f44e21942a2d888b152b459f
Beverly Hill <bjsrhill@gmail.com>
2013-07-06 21:43:53 +0000

    updated web.js to read index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 
+var fs = require('fs');
+
+var buffer = new buffer();
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buffer.toString('utc 8', fs.readFileSync('index.html')));
 });
 
 var port = process.env.PORT || 5000;
