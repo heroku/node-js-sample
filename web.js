5f0f89803ba76a09e29b402fc49b58f6a56b2ef1
Volodymyr Korobko <vladimir.korobko@gmail.com>
2013-07-07 02:38:38 +0000

    fs.readFileSync added

--- web.js
+++ web.js
@@ -1,12 +1,7 @@
 var express = require('express');
 
-var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
 });
