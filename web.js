ce4f42c776cee8c59a6304bf737313c326291615
John Smith <example@stanford.edu>
2013-07-07 19:18:12 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -11,5 +11,5 @@ app.get('/', function(request, response) {
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-    console.log("Listening on " + port);
+    console.log('Listening on ' + port);
 });
