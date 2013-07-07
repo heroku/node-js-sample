8a0e6eaf8969c02eb286bf65647c3ec8af081bb6
Volodymyr Korobko <vladimir.korobko@gmail.com>
2013-07-07 03:31:01 +0000

    new web.js

--- web.js
+++ web.js
@@ -1,12 +1,23 @@
+#!/usr/bin/env node
+
+var fs = require ("fs");
 var express = require('express');
+
+var inputfile = "index.html";
+var webout = fs.readFileSync('index.html');
+
+ console.log(webout);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(webout);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+
