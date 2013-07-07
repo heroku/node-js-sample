d9fb623f34c718daec7ce023d3691a93ac941f76
Jack Yang <jackyujieyang@gmail.com>
2013-07-07 21:13:48 +0000

    Updated checks.json and added URL option to grader.js

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -61,14 +62,27 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var download_url_content = function(url, checksfile) {
+    rest.get(url).on('success', function(data, response) {
+	var checkJson = checkHtmlFile(data, checksfile);
+	var outJson = JSON.stringify(checkJson, null, 4);
+    });
+    console.log(outJson);
+}
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <html_url>', 'URL to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if (process.url) {
+	download_url_content(process.url, program.checks);
+    } else {
+        var checkJson = checkHtmlFile(program.file, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
