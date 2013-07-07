8a8536e86de09ac9227115dc684ee75775d5a8ee
brimartin <brian_martin@alumni.grinnell.edu>
2013-07-07 20:21:37 +0000

    web.js random test

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf.toString(fs.readFileSync('index.html')))
+  response.send(fs.readFileSync('index.html').toString())
 });
 
 var port = process.env.PORT || 5000;
