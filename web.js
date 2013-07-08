3e6978c1b12a919b1ed7b8883c42b61ef686711b
hannes10 <hannes.herrmann@gmail.com>
2013-07-08 19:45:33 +0000

    new update web.js

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+var fs = require('fs');
 var c = fs.readFilesync('./index.html','utf8');
   response.send( c );
 });
