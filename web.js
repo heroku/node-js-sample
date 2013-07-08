227388dce27c4fe3177b506f9bb555136fdac067
hannes10 <hannes.herrmann@gmail.com>
2013-07-08 20:24:21 +0000

    syntax correction

--- web.js
+++ web.js
@@ -3,9 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-var fs = require('fs');
-var c = fs.readFileSync('./index.html','utf8');
-  response.send( c );
+var fs = require("fs"), sys=require("sys");
+var content = fs.readFileSync("./index.html","utf8");
+sys.put(JSON.parse(content));
 });
 
 var port = process.env.PORT || 5000;
