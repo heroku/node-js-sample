d706914b36d3bb3ba83e270b40b14af9e40843ad
hannes10 <hannes.herrmann@gmail.com>
2013-07-08 21:06:37 +0000

    all changed

--- web.js
+++ web.js
@@ -3,9 +3,15 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-var fs = require("fs"), sys=require("sys");
-var content = fs.readFileSync("./index.html","utf8");
-sys.put(JSON.parse(content));
+var fs = require('fs');
+fs.readFile('index.html', funtion (err, content) {
+if (err) {
+res.writeHead(300, {'Content-Type':'text/plain'});
+res.end('internal error: ' + err);
+} else {
+res.writeHead(200, {'Content-Type':'text/html'});
+res.end(content, 'utf-8');
+}
 });
 
 var port = process.env.PORT || 5000;
