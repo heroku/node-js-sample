e66cf55c03fad4634a09902d0cd073a52f78619a
hannes10 <hannes.herrmann@gmail.com>
2013-07-08 19:51:58 +0000

    watch uppercase syntax

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 var fs = require('fs');
-var c = fs.readFilesync('./index.html','utf8');
+var c = fs.readFileSync('./index.html','utf8');
   response.send( c );
 });
 
