e216ca8b4781baa74349530ae8efd2958436bf5d
hannes10 <hannes.herrmann@gmail.com>
2013-07-08 19:29:58 +0000

    Second updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+var c = fs.readFilesync('./index.html','utf8');
+  response.send( c );
 });
 
 var port = process.env.PORT || 5000;
