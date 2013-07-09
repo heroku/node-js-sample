7067417bc686ea6125f5bb150203e2d76f362efc
Alex Keramidas <alexkeramidas@gmail.com>
2013-07-09 14:19:17 +0000

    Update web.js, test with buffer

--- web.js
+++ web.js
@@ -3,11 +3,14 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-var fs = require('fs');
-    var k = fs.readFileSync('index.html', 'utf-8', function(err, data){
-	if(err) throw err;
-	console.log(data)});
-    response.send(k);
+    var fs = require('fs');
+
+    var k = fs.readFileSync('index.html', function(err, data){
+        if(err) throw err;
+        console.log(data)});
+    var buffer = new Buffer(k);
+
+    response.send(buffer.toString('utf-8',0, buffer.legth));
 });
 
 var port = process.env.PORT || 5000;
