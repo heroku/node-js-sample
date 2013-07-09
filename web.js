e29301ee9b660d42844f78d69f7e68c140863e00
Honey I'm Lost <github@honeyimlost.com>
2013-07-08 23:48:52 -0600

    Adding index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var data = fs.readFileSync('./index.html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2.0!');
+  response.send(data.toString());
 });
 
 var port = process.env.PORT || 5000;
