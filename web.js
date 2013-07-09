4f1604f3446f635f514c233389e47f95b778da15
xiangxu liu <iasonliu@gmail.com>
2013-07-09 09:17:36 +0000

    change web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
+var index = fs.readFileSync('index.html');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(index);
 });
 
 var port = process.env.PORT || 5000;
