4f410710d6cd613cd9a9d524e6e3db6078330542
hannes10 <hannes.herrmann@gmail.com>
2013-07-09 19:28:10 +0000

    only text

--- web.js
+++ web.js
@@ -5,7 +5,10 @@ var app = express.createServer(express.logger());
 var content = fs.readFileSync('./index.html');
 
 app.get('/', function(request, response) {
-response.send( content );
+// download file with content
+// response.send( content );
+// display content
+console.log (content);
 });
 
 var port = process.env.PORT || 5000;
