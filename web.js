8b6680a6ec5d5e9ec3806efd131da4247015492d
hannes10 <hannes.herrmann@gmail.com>
2013-07-09 05:10:24 +0000

    and another sample

--- web.js
+++ web.js
@@ -1,20 +1,10 @@
-var express = require('express');
+var http = require('http'),
+    fs = require('fs');
 
-var app = express.createServer(express.logger());
+var html = fs.readFileSync('./index.html');
 
-app.get('/', function(request, response) {
-var fs = require('fs');
-fs.readFile('index.html', funtion (err, content) {
-if (err) {
-res.writeHead(300, {'Content-Type':'text/plain'});
-res.end('internal error: ' + err);
-} else {
-res.writeHead(200, {'Content-Type':'text/html'});
-res.end(content, 'utf-8');
-}
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+http.createServer(function(request, response) {  
+    response.writeHeader(200, {"Content-Type": "text/html"});  
+    response.write(html);  
+    response.end();  
+}).listen(5000);
