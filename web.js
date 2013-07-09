ad506a48164bd4e8008dbde720865eaa60212241
hannes10 <hannes.herrmann@gmail.com>
2013-07-09 18:48:48 +0000

    with express

--- web.js
+++ web.js
@@ -1,11 +1,14 @@
-#!/usr/bin/env node
-var http = require('http'),
-    fs = require('fs');
+var express = require('express');
+fs = require('fs');
 
-var html = fs.readFileSync('./index.html');
+var app = express.createServer(express.logger());
+var content = fs.readFileSync('./index.html');
 
-http.createServer(function(request, response) {  
-    response.writeHeader(200, {"Content-Type": "text/html"});  
-    response.write(html);  
-    response.end();  
-}).listen(5000);
+app.get('/', function(request, response) {
+response.send( content );
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+console.log("Listening on " + port);
+});
