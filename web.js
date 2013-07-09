fcea35b0cc47b9af16f63292149d7429fc329891
Alex Keramidas <alexkeramidas@gmail.com>
2013-07-09 14:12:05 +0000

    web.js Restart reading from file

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 var fs = require('fs');
-    var k = fs.readFileSync('index.html', function(err, data){
+    var k = fs.readFileSync('index.html', 'utf-8', function(err, data){
 	if(err) throw err;
 	console.log(data)});
     response.send(k);
