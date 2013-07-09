200ec661c995fb958146f199fa95d4b549bcc371
Alex Keramidas <alexkeramidas@gmail.com>
2013-07-09 13:53:50 +0000

    web.js Restart reading from file

--- web.js
+++ web.js
@@ -2,8 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
+var buffer = new Buffer(8);
+
 app.get('/', function(request, response) {
-   response.send('Hello World 2!');
+    fs.readFileSync('index.html', function(err, data){
+	if(err) throw err;
+	console.log(data)});
+    response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
