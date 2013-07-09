f561eefc172a40f5879df124584285b34484e27f
Alex Keramidas <alexkeramidas@gmail.com>
2013-07-09 13:35:40 +0000

    Modified web.js to read from file

--- web.js
+++ web.js
@@ -4,10 +4,7 @@ var fs = require('fs');
 var buffer = new Buffer(8);
 
 app.get('/', function(request, response) {
-    fs.readFileSync('index.html', function(err,data){
-    if(err) throw err;
-    console.log(data)});
-    Response.send('Hello World 2!');
+   Response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
