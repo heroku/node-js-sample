e652b2b7ce7289880eb9dad067468f56ba15a0b7
Alexander Keramidas <dev.alexkeramidas@gmail.com>
2013-07-09 16:49:56 +0300

    Web.js Restart

--- web.js
+++ web.js
@@ -1,8 +1,9 @@
 var express = require('express');
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-   Response.send('Hello World 2!');
+  response.send('Hello World!');
 });
 
 var port = process.env.PORT || 5000;
