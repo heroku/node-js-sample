8e77388646517fa09e35b657f49bd93e624fe415
Harry Botboy <harrybotboy@gmail.com>
2013-07-10 02:21:17 +0000

    added index.html NT

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World from index.html');
+  var buffer = new Buffer("Hello World from index.html", 25);
+  fs.readFileSync("index.html", "utf-8");
+  response.send(resultOfreadFileSync);
 });
 
 var port = process.env.PORT || 5000;
