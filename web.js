b1c5fdca4dc34309507c96b48817756cbf81bb14
Zhong Chen <zchen87@gmail.com>
2013-07-10 00:11:21 -0700

    index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
+fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var buffer = fs.readFileSync("index.html");
+  var string = buffer.toString();
+  response.send(string);
 });
 
 var port = process.env.PORT || 5000;
