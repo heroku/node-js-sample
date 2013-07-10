6a8119388aaa3624cca5a9666315f19feb52a7f7
Harry Botboy <harrybotboy@gmail.com>
2013-07-10 03:18:04 +0000

    utf-8

--- web.js
+++ web.js
@@ -1,11 +1,13 @@
+#!/usr/bin/env node
+   
 var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var my_index = fs.readFileSync("index.html");
-  response.send(my_index.toString('utf-8));
+    var my_index = fs.readFileSync("index.html");
+  response.send(my_index.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
