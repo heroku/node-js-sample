b145102e66f2697ce912d0778cfc7c12ce2b591e
harrybotboy <hoang.ktxd@gmail.com>
2013-07-10 20:41:10 +0000

    Copy of Minh

--- web.js
+++ web.js
@@ -1,16 +1,107 @@
 #!/usr/bin/env node
-   
-var express = require('express');
+/*
+Automatically grade files for the presence of specified HTML tags/attributes.
+Uses commander.js and cheerio. Teaches command line application development
+and basic DOM parsing.
+
+References:
+
+ + cheerio
+   - https://github.com/MatthewMueller/cheerio
+   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
+   - http://maxogden.com/scraping-with-node.html
+
+ + commander.js
+   - https://github.com/visionmedia/commander.js
+   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
+
+ + JSON
+   - http://en.wikipedia.org/wiki/JSON
+   - https://developer.mozilla.org/en-US/docs/JSON
+   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
+*/
+
 var fs = require('fs');
+var program = require('commander');
+var cheerio = require('cheerio');
+var rest = require('restler');
+var util = require('util');
+var HTMLFILE_DEFAULT = "index.html";
+var CHECKSFILE_DEFAULT = "checks.json";
 
-var app = express.createServer(express.logger());
+var assertFileExists = function(infile) {
+    var instr = infile.toString();
+    if(!fs.existsSync(instr)) {
+        console.log("%s does not exist. Exiting.", instr);
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+    }
+    return instr;
+};
 
-app.get('/', function(request, response) {
-    var my_index = fs.readFileSync("index.html");
-    response.send(my_index.toString('utf-8'));
-});
+var loadChecks = function(checksfile) {
+    return JSON.parse(fs.readFileSync(checksfile));
+};
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+var checkHtmlFile = function(htmlfile, checksfile) {
+    return check(fs.readFileSync(htmlfile), checksfile);
+};
+
+var checkURL = function(url, checksfile, callback) {
+    var checkResponse = function(result, response) {
+      if (result instanceof Error) {
+        message = "no response";
+        if (response) {
+          message = util.format(response.message);
+        }
+        console.error('Error: ' + message);
+      } else {
+        console.log('URL "' + url + '" loaded successfully');
+        result = check(result, checksfile);
+      }
+      callback(result);
+    }
+    rest.get(url).on('complete', checkResponse);
+};
+
+var check = function(html, checksfile) {
+    $ = cheerio.load(html);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
+
+var clone = function(fn) {
+    // Workaround for commander.js issue.
+    // http://stackoverflow.com/a/6772648
+    return fn.bind({});
+};
+
+if(require.main == module) {
+    program
+        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists))
+        .option('-u, --url <html_url>', 'URL to index.html')
+        .parse(process.argv);
+    if (program.file) {
+      var checkJson = checkHtmlFile(program.file, program.checks);
+      var outJson = JSON.stringify(checkJson, null, 4);
+      console.log(outJson);
+    } else if (program.url) {
+      checkURL(program.url, program.checks, function(checkJson) {
+        if (checkJson instanceof Error) {
+          console.error('Check failed');
+        } else {
+          var outJson = JSON.stringify(checkJson, null, 4);
+          console.log(outJson);
+        }
+      });
+    } else {
+      checkJson = "error: either a file path or an URL must be provided";
+    }
+} else {
+    exports.checkHtmlFile = checkHtmlFile;
+}
