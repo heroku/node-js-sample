a2b04ea9487adfaf16b03b8ae11018e0c437672a
Rohit Parulkar <rohit.parulkar@gmail.com>
2013-07-10 11:52:34 +0000

    modifying to take hello world from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var buf = fs.readFileSync('index.html');
+var finalString = buf.toString('utf-8',0,buf.length-1);
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(finalString);
 });
 
 var port = process.env.PORT || 5000;
