a88a94d03b944a10a18d359f0d3f44d49cbe53c0
Marius Encher <marius_encher@yahoo.com>
2013-07-10 23:41:06 +0300

    Serve index.html

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.sendfile(__dirname + '/index.html');
 });
 
 var port = process.env.PORT || 5000;
