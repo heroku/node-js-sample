3800658a1f937601e890da12048aed77a0102f3f
Brian Perin <bperin42@gmail.com>
2013-07-11 01:06:07 +0000

    syntax

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var http = require('http'),
     fs = require('fs');
 
 
-fs.readFile('./index.html', function (err, html) {
+fs.readFile('index.html', function (err, html) {
     if (err) {
         throw err; 
     }       
