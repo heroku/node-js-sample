33e1ea614524cb71361cd5c92baf6f00e50399a6
Brian Perin <bperin42@gmail.com>
2013-07-11 00:56:56 +0000

    Switched to index.html

--- web.js
+++ web.js
@@ -1,12 +1,14 @@
-var express = require('express');
+var http = require('http'),
+    fs = require('fs');
 
-var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World! 2');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8000);
 });
