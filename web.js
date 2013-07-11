315054a587fce5e320ff3b01b9e203996fe73dc2
Ubuntu <ubuntu@ip-172-31-4-185.us-west-2.compute.internal>
2013-07-11 08:03:47 +0000

    Modify to read html file

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var readFile = fs.readFileSync('index.html');
+var readFileString = readFile.toString();
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(readFileString);
 });
 
 var port = process.env.PORT || 5000;
