44f2bd8e8c3abf002a337562b65351f3cb2a961e
juvemark <juvemark@gmail.com>
2013-07-11 19:57:07 +0000

    Modify web.js

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var i=fs.readfile("index.html")
+
+var str = i.toString() 
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
