2d2d0de9841c618a3b78ee9e0ef310f69ea670aa
bperin <bperin42@gmail.com>
2013-07-10 22:48:24 -0700

    back to original

--- web.js
+++ web.js
@@ -1,14 +1,12 @@
-var http = require('http'),
-    fs = require('fs');
+var express = require('express');
 
+var app = express.createServer(express.logger());
 
-fs.readFile('index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(8000);
+app.get('/', function(request, response) {
+  response.send('Hello World! 2');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
