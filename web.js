eb8f55aad2c4d459841244a641b3bbcc057ad97d
juvemark <juvemark@gmail.com>
2013-07-11 21:18:39 +0000

    Mofity web.js

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+  var fs = require('fs');
   var i = fs.readFileSync('index.html', 'utf-8');
   response.send(i.toString());
 });
