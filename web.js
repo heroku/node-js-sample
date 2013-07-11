669b3fe3f3059ba4f2c5f3765eb5d024f2f911ec
Benjamin Goegge <bgoegge@gmail.com>
2013-07-11 18:00:41 +0200

    Serving index.html through web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 
+var fs = require('fs');
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+	var buf = fs.readFileSync('index.html');
+	buf = buf.toString();
+	response.send(buf);
 });
 
 var port = process.env.PORT || 5000;
