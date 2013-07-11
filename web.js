76061f420beee79bffa09cbe6ba11c7b9af15456
Ubuntu <ubuntu@ip-10-252-30-73.us-west-2.compute.internal>
2013-07-11 19:24:01 +0000

    updated web.js to read from index.html

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ app.get('/', function(request, response) {
 //  response.send('Hello World 2!');
   var fs = require('fs');
   var mybuffer;
-  var k = fs.readFileSync('/index.html','utf8');
+  var k = fs.readFileSync('./index.html','utf8');
   response.send(k.toString());
 });
 
