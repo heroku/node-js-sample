69b211bf97a47648b50660d7181dad23fadd4f09
joao <joao.a.veiga@gmail.com>
2013-07-11 00:37:54 +0000

    Modified to read from html.

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
 
+var fs = require('fs');
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var buff = fs.readFileSync('index.html');
+  response.send(buff.toString());
 });
 
 var port = process.env.PORT || 5000;
