b36ca2d79752fed19f82d05da3dc2954ac5a1b97
Felix Dorrek <felix.dorrek@gmail.com>
2013-07-13 22:43:28 +0200

    read-html3

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
  var buffer = new Buffer(fs.readFileSync('./index.html'));
-console.log(buffer.toString('utf-8'));
+response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
