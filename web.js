b85ac328494497e37dc3c782dd3e85df469c6c5f
JP Olivier <JPolivier42@gmail.com>
2013-07-12 23:46:37 -0700

    Should be working now. Last JP attempt

--- web.js
+++ web.js
@@ -2,15 +2,16 @@ var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
-var buffer = fs.readFileSync('/index.html');
+var buffer = fs.readFileSync('index.html');
 
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString);
+  response.send(buffer.toString());
   response.send('testing');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-});
+})
+;
