8ae389b3ba2d8d5ac2304666072083b18132febb
Felix Dorrek <felix.dorrek@gmail.com>
2013-07-13 22:02:43 +0200

    read-html1

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  fs.readFileSync('index.html').toString('utf8');
 });
 
 var port = process.env.PORT || 5000;
