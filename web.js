06146aa03cc967e24cff5dd65f5525dce0ea82dd
Hunter Gemmer <hunterg325@gmail.com>
2013-07-14 02:12:01 +0000

    updated web.js

--- web.js
+++ web.js
@@ -7,8 +7,8 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
      
       var fs = require('fs');
-      var buffer = new Buffer();
-      fs.readFileSync("index.html"); 
+      var buffer = new Buffer("index.html");
+       
          
      response.send(buffer.toString('utf-8'));
 });
