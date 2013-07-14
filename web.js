1b599d4926c6cce82eb6d8d412cc38d746b46e8f
Hunter Gemmer <hunterg325@gmail.com>
2013-07-14 02:17:06 +0000

    fixed issues with buffer

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
      
       var fs = require('fs');
-      var buffer = new Buffer("index.html");
+      var buffer = new Buffer(fs.readFileSync('index.html');
        
          
      response.send(buffer.toString('utf-8'));
