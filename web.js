fe5aef6940e7d4e700d2f2dbc7959ab7dd912046
Hunter <hunterg325@gmail.com>
2013-07-13 21:18:59 -0500

    Update web.js

--- web.js
+++ web.js
@@ -1,4 +1,3 @@
-
 var express = require('express');
 
 
@@ -7,8 +6,8 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
      
       var fs = require('fs');
-      var buffer = new Buffer();
-      fs.readFileSync("index.html"); 
+      var buffer = new Buffer(fs.readFileSync("index.html"));
+      
          
      response.send(buffer.toString('utf-8'));
 });
