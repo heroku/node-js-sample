2a628feb645d6ca1033796a33f026c8c605af2a6
Beverly Hill <bjsrhill@gmail.com>
2013-07-14 15:41:08 +0000

    Modified web.js to listen on port 8080, refactored index.html to incorporate Twitter Bootstrap and enable responsive design.

--- web.js
+++ web.js
@@ -1,16 +1,13 @@
 var express = require('express');
-
 var fs = require('fs');
-
-var buffer = new Buffer(fs.readFileSync('index.html'));
-
+var htmlfile = "index.html";
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString('utf8', 0, buffer.length));
+  var html = fs.readFileSync(htmlfile).toString();
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
