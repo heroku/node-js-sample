c96b41e4a331cb2ed12f1db2c8de39375456443a
Matthew McGivney <matthew.mcgivney@gmail.com>
2013-07-14 09:26:01 -0400

    Updated web.js to return contents of index.html

--- web.js
+++ web.js
@@ -1,9 +1,35 @@
+/*####################
+From node.js API docs (http://nodejs.org/api/fs.html#fs_fs_readfilesync_filename_options)
+fs.readFileSync(filename, [options])
+	Synchronous version of fs.readFile.  Returns the contents
+	of the parameter filename.
+
+	If the encoding option is specified then this function
+	returns a string.  Otherwise, it returns a buffer.
+####################*/
+
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+
+/*####################
+From node.js API docs (http://nodejs.org/api/fs.html#fs_fs_readfilesync_filename_options)
+fs.readFileSync(filename, [options])
+	Synchronous version of fs.readFile.  Returns the contents
+	of the parameter filename.
+
+	If the encoding option is specified then this function
+	returns a string.  Otherwise, it returns a buffer.
+####################*/
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	//get the contents of index.html
+	var contents = fs.readFileSync("index.html");
+
+	//response.send(string from previous step)
+  response.send(contents.toString());
 });
 
 var port = process.env.PORT || 5000;
