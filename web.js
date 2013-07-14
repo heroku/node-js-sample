04bee367eadda371ec58f07ac1e3f6b86851be11
Carl Hinkle <carl@pleasefindmyanimal.com>
2013-07-14 02:19:57 +0000

    Starting to move to new homewok state.  Some code organization, moved code, css, js, and img to subdirectories of public.  Added express.static directory to allow access to the public directory.  (If there's time explore adding ngnix front end for these static resources)

--- web.js
+++ web.js
@@ -2,12 +2,14 @@ var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(request, response) {
-    var buffer = fs.readFileSync('index.html');
+    var buffer = fs.readFileSync('public/src/index.html');
     response.send(buffer.toString('utf-8'));
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
