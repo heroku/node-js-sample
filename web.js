b52dd04c4d240936c35303dd92d0b6e4fc4ab36b
krishnaiitd <kprasad.iitd@gmail.com>
2013-07-14 10:26:00 +0000

    Changes message display in web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Hello World! from krishna prasad');
 });
 
 var port = process.env.PORT || 5000;
