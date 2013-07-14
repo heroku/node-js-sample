be19e02ceeef9fc3cb6be2731210161c89bd58c8
Sangam Chopade <sangamstart@gmail.com>
2013-07-14 14:53:17 +0530

    Modified web.js to listen 8080 and index.html

--- web.js
+++ web.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var fs = require('fs');
-var buf = new Buffer(fs.readFileSync("index.html"));
 var app = express.createServer(express.logger());
+var buf = new Buffer(fs.readFileSync("index.html","utf8"));
 
 app.get('/', function(request, response) {
-  response.send(buf.toString());
-});
+   response.send(
+    buf.toString()    );
+ });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
