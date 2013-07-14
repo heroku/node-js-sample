3f55c010384e477fce74fa1e8aeb7ad1deadea35
Beverly Hill <bjsrhill@gmail.com>
2013-07-14 21:19:34 +0000

    changing port back to 5000

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ app.get('/', function(request, response) {
   var html = fs.readFileSync(htmlfile).toString();
 });
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
