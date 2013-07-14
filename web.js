29849eb86787f3b3a2bc5022a59675b931106fbf
Rafael Fernandes <rafaelfernandesmarcondes@gmail.com>
2013-07-14 22:52:09 +0000

    v2 - added buffer

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
+var content = fs.readFileSync('index.html', 'utf-8');
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
