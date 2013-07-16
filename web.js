2c67efa819eec386aac922fb9cd1061f076d9570
joao <joao.a.veiga@gmail.com>
2013-07-16 21:46:55 +0000

    Minor code update for web.js

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
 var express = require('express');
-
 var fs = require('fs');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buff = fs.readFileSync('index.html');
-  response.send(buff.toString());
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
 var port = process.env.PORT || 8080;
