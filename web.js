d551a7ff0f4da5221998c9901a1048fba234ebbc
Rafael Fernandes <rafaelfernandesmarcondes@gmail.com>
2013-07-16 22:38:36 +0000

    v4 equal to stanford

--- web.js
+++ web.js
@@ -2,13 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 var fs = require('fs');
-var content = fs.readFileSync('index.html', 'utf-8');
+var html = "index.html";
 
 app.get('/', function(request, response) {
-  response.send(content);
+  var html = fs.readFileSync(htmlfile).toString();  
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
