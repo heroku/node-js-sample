c813a58a45896f58416d2ab9857e7577c90605c9
Matthew McGivney <matthew.mcgivney@gmail.com>
2013-07-16 20:10:33 -0400

    Updated restlerTest to take command line arguments and pass them to the saveHtmlToFile function.

--- restlerTest.js
+++ restlerTest.js
@@ -1,12 +1,47 @@
 #!/usr/bin/env node
 
-/*Pulls html from http://google.com and saves it to file temp.html*/
+/*Pulls html from http://google.com and saves it to file temp.html
+Utilizes restler library for get() call.
+Utilizes commander library for .option functionality
+
+*/
+
+
 
 var sys = require('util'),
     rest = require('restler'),
+    program = require('commander'),
     fs = require('fs');
 
+function saveHtmlToFile (url, filename) {
+	rest.get(url).on('complete', function(result) {
+	  if (result instanceof Error) {
+	    sys.puts('Error: ' + result.message);
+	    this.retry(5000); // try again after 5 sec
+	  } else {    
+	    fs.writeFileSync(filename, result);
+	  }
+	});
+}
 
+/*program with the option -u or --url which takes a given URL and saves the file to the option -f or --file*/
+if(require.main == module) {
+    program
+        //.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html')//, clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL')//todo:define url and add clone(assert) call and add URL_DEFAULT
+        .parse(process.argv);
+    //var checkJson = checkHtmlFile(program.file, program.checks);
+    //var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(program.url);
+    if (program.url) saveHtmlToFile(program.url, program.file);
+} else {
+    console.log("Else.");
+    //exports.checkHtmlFile = checkHtmlFile;
+}
+
+
+/*// 2013-07-17 This is the code from before I cut/pasted into the function.
 
 rest.get('http://google.com').on('complete', function(result) {
   if (result instanceof Error) {
@@ -16,5 +51,5 @@ rest.get('http://google.com').on('complete', function(result) {
     
     fs.writeFileSync("temp.html", result);
   }
-});
+});*/
 
