a44d24d3d7415c27fbaf750ffea7004acfc95529
Hashan Punchihewa <hashan.punchihewa@gmail.com>
2013-07-18 19:41:11 +0000

    Updated index.html

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ app.get('/', function(request, response) {
   response.send(fs.readFileSync("index.html").toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
