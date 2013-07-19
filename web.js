b87d20b5b8d71b8630ea63e29d0c0182613b1da0
ABS <carlosabs@gmail.com>
2013-07-19 12:11:11 +0000

    Hoemwork 3 web.js

--- web.js
+++ web.js
@@ -3,10 +3,13 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+    fs.readFile('/etc/passwd', function (err, data) {
+	  if (err) throw err;
+	  response.send(data.toString());
+	});
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-});
+};)
