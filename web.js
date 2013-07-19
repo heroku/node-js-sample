ba9a0150066e7736c2d59c41f01d01ee92f74f65
harrybotboy <hoang.ktxd@gmail.com>
2013-07-19 04:24:03 +0000

    rewrite html

--- web.js
+++ web.js
@@ -2,12 +2,13 @@
    
 var express = require('express');
 var fs = require('fs');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var my_index = fs.readFileSync("index.html");
-    response.send(my_index.toString('utf-8'));
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
 });
 
 var port = process.env.PORT || 8080;
