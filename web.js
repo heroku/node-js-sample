f10014c96fa99f3e0163c4cb1ba4a2721b93d21d
Ubuntu <ubuntu@ip-10-250-9-86.us-west-2.compute.internal>
2013-07-19 21:01:47 +0000

    Added scaffholding and placeholders to layout and change listening port to 8080.

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ app.get('/', function(request, response) {
   response.send(data);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
