2fdf3fd4f59d9dd783649d7f1ba8e70103ee72f0
Ubuntu <ubuntu@ip-172-31-17-83.us-west-2.compute.internal>
2013-07-20 11:14:52 +0000

    changes port to 8080

--- web.js
+++ web.js
@@ -5,14 +5,15 @@ var app = express.createServer(express.logger());
 var fs = require('fs');
 
 app.get('/', function(request, response) {
-  
+
   data = fs.readFileSync('index.html');
-  strdata = data.toString('utf8');	
+  strdata = data.toString('utf8');
  // response.send('Hello World! from krishna prasad');
   response.send(strdata);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
