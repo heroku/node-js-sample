5c7e304b418fcdc98312ad7d1d699978d137bf81
streriling <stoqn.stefanov87@gamil.com>
2013-07-20 18:33:56 +0000

    Updated web.js with index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs')
 
 var app = express.createServer(express.logger());
+var buf = fs.readFileSync('index.html')
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
