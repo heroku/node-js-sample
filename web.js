d3b8b9ca2a3ec68bf382ac543c0f52119da7e281
Matthew McGivney <matthew.mcgivney@gmail.com>
2013-07-20 20:19:53 +0000

    Part 2 ready for submission.

--- web.js
+++ web.js
@@ -32,7 +32,7 @@ app.get('/', function(request, response) {
   response.send(contents.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
