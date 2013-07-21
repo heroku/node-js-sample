896532ce7b5624396e75a2f5fa977ee35d550ffc
Don Kendrick <me@donkendrick.org>
2013-07-21 12:57:42 +0000

    changed location of images

--- web.js
+++ web.js
@@ -8,6 +8,8 @@ app.get('/', function(request, response) {
   response.send(buf.toString());
 });
 
+app.use('/images', express.static(__dirname + '/images'));
+
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
