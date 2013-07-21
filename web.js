f2f379fc0e567e139e705101826ca5a2cd4f17bb
Rezan Achmad <rezanachmad@gmail.com>
2013-07-21 05:39:19 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
