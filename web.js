507c031708d209522b745bbc7968194a9a0e1bc3
Rezan Achmad <rezanachmad@gmail.com>
2013-07-21 05:49:33 +0000

    modify web.js to read index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var buffer = fs.readFileSync('index.html');
+  var string = buffer.toString();
+  response.send(string);
 });
 
 var port = process.env.PORT || 5000;
