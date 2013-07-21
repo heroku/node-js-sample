0be8247b40855e4e4f643e65472f2e207c26f6f0
streriling <stoqn.stefanov87@gamil.com>
2013-07-21 17:36:18 +0000

    Added fixed web.js

--- web.js
+++ web.js
@@ -1,8 +1,8 @@
 var express = require('express');
-var fs = require('fs')
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
-var buf = fs.readFileSync('index.html')
+var buf = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
   response.send(buf.toString());
