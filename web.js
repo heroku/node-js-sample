b9e66e3cbb89191f32f5aaf54e0c1e4c8c108b6f
Danish Arshad <danisharshad@gmail.com>
2013-07-22 14:53:14 -0700

    updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Hello World2!');
 });
 
 var port = process.env.PORT || 5000;
