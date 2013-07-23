98205c77f931f93f54642eb935f878e3f7a36a69
streriling <stoqn.stefanov87@gamil.com>
2013-07-23 21:50:35 +0000

    Fixed version

--- grader.js
+++ grader.js
@@ -23,10 +23,12 @@ References:
 
 var fs = require('fs');
 var program = require('commander');
-var cheerio = require('cheerio');
 var rest = require('restler');
+var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var DLFILE_DEFAULT = "temp.html";
+
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -37,18 +39,8 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var assertUrlFormatted = function(inUrl) {
-    var url = inUrl;
-    //TODO validate URL
-    return url;
-};
-
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
-};
-
-var cheerioHtmlContent = function(htmlContent) {
-    return cheerio.load(htmlContent);
 };
 
 var loadChecks = function(checksfile) {
@@ -66,51 +58,36 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
-var checkHtmlSource = function(htmlContent, checksfile) {
-    $ = cheerioHtmlContent(htmlContent);
-    var checks = loadChecks(checksfile).sort();
-    var out = {};
-    for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
-    }
-    return out;
-};
-
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
 
-var processHTMLSource = function(htmlSource, checks) {
-    var checkJson = checkHtmlSource(htmlSource, checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
-}
-
 if(require.main == module) {
     program
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), "")
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-u, --url <check_file>', 'Url to source html', clone(assertUrlFormatted), "")
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL to download')
         .parse(process.argv);
-    if (program.file && program.checks) {
-	var htmlSource = fs.readFileSync(program.file)
-	    processHTMLSource(htmlSource, program.checks);
-    } else if (program.url && program.checks) {
-	rest.get(program.url).on('complete', function(result, response) {
-            if (result instanceof Error) {
-                console.error('Error: ' + util.format(response.message));
-            } else {
-                console.log("Wrote %s", result);                
-		    processHTMLSource(result, program.checks);
-            }
-        });
-    } else {
-	//TODO add proper usage.
-	console.log("Invalid source specified");
-    }
+    
+    if(program.url) {
+  rest.get(program.url).on('complete', function(result) {
+  if (result instanceof Error) {
+    console.error('Error: ' + result.message);
+  } else {
+     fs.writeFileSync(DLFILE_DEFAULT,result);
+     var checkJson = checkHtmlFile(DLFILE_DEFAULT, program.checks);   
+     var outJson = JSON.stringify(checkJson, null, 4);
+     console.log(outJson)
+  }
+});
+   } 
+   else{ 
+      var checkJson = checkHtmlFile(program.file, program.checks);
+      var outJson = JSON.stringify(checkJson, null, 4);
+      console.log(outJson);
+  } 
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
