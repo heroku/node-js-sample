e48f512aca6e05cf13f0131f75122c365857f954
hazarizk <harilazan@gmail.com>
2013-07-26 08:14:06 +0000

    edited product name in index.html

--- web.js
+++ web.js
@@ -10,7 +10,7 @@ app.get('/', function(request, response) {
 
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
