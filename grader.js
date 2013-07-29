e2def756324a7a8ed4f1491cbc47b8a4f78163c4
Ubuntu <ubuntu@ip-172-31-24-221.sa-east-1.compute.internal>
2013-07-29 23:15:40 +0000

    parsing working

--- grader.js
+++ grader.js
@@ -26,6 +26,9 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var rest = require('restler');
+var url = "http://www.pontofrio.com.br/";
+
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -61,14 +64,43 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var checkUrl = function(url, checksfile) {
+$ = cheerio.load(url);
+var checks = loadChecks(checksfile).sort();
+var out = {};
+for(var ii in checks) {  var present = $(checks[ii]).length > 0;
+out[checks[ii]] = present; }
+return out;
+};
+
+
+
+
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_file>', 'URL')
         .parse(process.argv);
+
+if(program.url) { rest.get(program.url).on('complete', function(result) { 
+
+if(result instanceof Error) { console.log("Page not found");
+process.exit(1);}
+
+else { console.log("Checking"  + program.url);
+console.log(checkUrl(result, program.checks));}});
+
+
+}
+
+
+else { 
+
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    console.log(outJson); }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
