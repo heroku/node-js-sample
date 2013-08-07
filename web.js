9930a4aa82d882f06df91ce0ec3dfb5d2310c57e
Henry Haller <henrybhaller@gmail.com>
2013-08-07 19:11:07 +0000

    updated index.html

--- web.js
+++ web.js
@@ -9,7 +9,7 @@ app.get('/', function(request, response) {
   response.send(fs.readFileSync("index.html").toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8081;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
