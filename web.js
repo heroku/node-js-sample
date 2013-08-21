f6b504483e68c920a8ef67e103a96d8c68ddd579
Ubuntu <ubuntu@ip-10-179-45-199.ec2.internal>
2013-08-21 23:21:28 +0000

    Modified web.js to variable index.html

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var buf = new Buffer(fs.readFileSync("index.html"));
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
