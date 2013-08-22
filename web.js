40510c5c2aeea0e6572dd0fddcfc99929722c040
kyalaman <kyalaman@alumni.princeton.edu>
2013-08-22 23:12:13 +0000

    Edited product desc in index.html

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ app.get('/', function(request, response) {
   response.send(buf.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
