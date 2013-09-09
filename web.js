f6443f2f538cb220ac6df0d97a125e54172e8c0e
Felix Dorrek <felix.dorrek@gmail.com>
2013-09-09 08:29:34 +0000

    port 8080

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ app.get('/', function(request, response) {
 response.send(buffer.toString('utf-8'));
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
