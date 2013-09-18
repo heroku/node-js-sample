ea9d3ac3516aa206be461a288751dbc51848a831
mdeboc <mdeboc@zoho.com>
2013-09-18 10:36:30 +0000

    retour en arriere a cause d un pepin avec fs.readFileSync

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync(index.html, [encoding)]);
+  response.send("Hello World 3!");
 });
 
 var port = process.env.PORT || 5000;
