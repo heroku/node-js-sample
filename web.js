31a669ec4c0803b6c146180520004d23c61a3bd8
mdeboc <mdeboc@zoho.com>
2013-09-18 10:26:10 +0000

    encoding option crochet

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync(index.html, encoding));
+  response.send(fs.readFileSync(index.html, [encoding)]);
 });
 
 var port = process.env.PORT || 5000;
