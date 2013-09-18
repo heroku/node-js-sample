c0bd74bc1a258ebf900c99d05e67273f10b40b14
mdeboc <mdeboc@zoho.com>
2013-09-18 10:22:40 +0000

    Updated web.js fct fs.readFileSync

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(fs.readFileSync(index.html));
 });
 
 var port = process.env.PORT || 5000;
