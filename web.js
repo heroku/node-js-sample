c3a69b154496d9bcaad253f8be07a60e2dcafbee
Ubuntu <ubuntu@ip-172-31-30-5.eu-west-1.compute.internal>
2013-09-18 12:34:57 +0000

    data

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var data = "Hello World 3!";
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send("Hello World 3!");
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
