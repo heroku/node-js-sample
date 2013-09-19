be4ab788cc2976a240264ebf16d2115f98b98ad8
Ubuntu <ubuntu@ip-172-31-35-96.eu-west-1.compute.internal>
2013-09-19 09:22:27 +0000

    fs.readFileSync(data).toString()

--- web_old.js
+++ web_old.js
@@ -1,12 +1,11 @@
 var express = require('express');
-var fs = require('fs');
-var htmlfile = "index.html";
-
-var app = express.createServer(express.logger());
+var fs = require("fs");
+var data = fs.readFileSync("index.html", "utf8");
+var app = express();
+app.use(express.logger());
 
 app.get('/', function(request, response) {
-  var html = fs.readFileSync(htmlfile).toString();
-  response.send(html);
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
