b42c83269114a5ea07bdcf6fbefd1dd22e4e7807
Gabriel Tomitsuka <gabriel.tomitsuka@montreus.com>
2014-12-31 11:33:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express();
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 3000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
@@ -9,5 +9,5 @@ app.get('/', function(request, response) {
 })
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+  console.log("Node app is running at server:" + app.get('port'))
 })
