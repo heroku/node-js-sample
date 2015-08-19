4aae89fc10d9b79ff9668dad8449f13fe13c11ea
Aaron Spring <aaron.spring@gmx.de>
2015-08-19 21:45:08 +0000

    finally bitstarter template done

--- index.js
+++ index.js
@@ -10,13 +10,6 @@ var buffer = new Buffer(25600)
 
 var website = fs.readFileSync('index.html', 'utf8') 
 
-
-
-//buffer.write(fs.readFileSync('index.html'))
-
-//buffer.write('ausprobiert!')
-//website = buffer.toString('ascii')
-
 app.get('/', function(request, response) {
   response.send(website)
 })
