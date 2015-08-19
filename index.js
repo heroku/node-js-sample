089153207496922664a057f2d6451736a5eb66d2
Aaron Spring <aaron.spring@gmx.de>
2015-08-19 21:08:04 +0000

    habss unterschtzt

--- index.js
+++ index.js
@@ -4,8 +4,24 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+
+var fs = require('fs');
+var buffer = new Buffer(256) 
+
+website = buffer.toString('ascii')
+
+
+
+fs.readFileSync('index.html', function (err,data) {
+    if (err) throw err;
+    console.log(data);
+});
+
+
+
+
 app.get('/', function(request, response) {
-  response.send(index.html)
+  response.send(website)
 })
 
 app.listen(app.get('port'), function() {
