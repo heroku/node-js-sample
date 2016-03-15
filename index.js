84c913a2e26b01f1e5cf8cbe5783a3c656b6c747
sylwolveryn <marcell@neszt.hu>
2016-03-15 21:50:28 +0100

    bootstrap creative added

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ var app = express()
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/pub'))
 
 app.get('/', function(request, response) {
   response.send('Hello World!')
+  console.log("request: " + request);
+})
+
+app.get('/frontend', function(request, response) {
+  response.send('FrontEnd!');
+  console.log("Frontend initialized");
+  console.log("request: " + request);
 })
 
 app.listen(app.get('port'), function() {
