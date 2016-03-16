83ae3b0edd21bb622403f4726209f3a1e8259d1d
sylwolveryn <marcell@neszt.hu>
2016-03-16 02:18:01 +0100

    json files for quizzes, backend and frontend update - basic functionailties implemented

--- index.js
+++ index.js
@@ -1,21 +1,75 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var javascriptQuiz = JSON.parse(fs.readFileSync('json/javascriptQuiz.json', 'utf8'));
+var jdk8Quiz = JSON.parse(fs.readFileSync('json/jdk8Quiz.json', 'utf8'));
+var jdk9Quiz = JSON.parse(fs.readFileSync('json/jdk9Quiz.json', 'utf8'));
+var freemarkerQuiz = JSON.parse(fs.readFileSync('json/freemarkerQuiz.json', 'utf8'));
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 app.use(express.static(__dirname + '/pub'))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', function(request, response) {
   response.send('Hello World!')
-  console.log("request: " + request);
+  console.log("request: " + request.body);
 })
 
 app.get('/frontend', function(request, response) {
   response.send('FrontEnd!');
   console.log("Frontend initialized");
-  console.log("request: " + request);
+  console.log("request: " + request.body);
+})
+
+app.post('/submit', function(request, response) {
+  response.send('{data: "tbd"}');
+  console.log("/SUBMIT");
+  console.log("request: " + JSON.stringify(request.body, censor(request.body)));
+})
+
+app.post('/quiz-select', function(request, response) {
+  console.log("/quiz-select");
+  if (!request.body || !request.body.data) {
+    console.log("error, invalid request");
+    response.send({error: true, message: "invalid request", subMessage: "Please use the valid quizzes, do not try to come up with new ones :)"});
+  } else {
+      var selectedQuiz = request.body.data;
+      console.log("selectedQuiz: " + selectedQuiz);
+      response.send(loadQuiz(selectedQuiz));
+  }
 })
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+
+function censor(censor) {
+  var i = 0;
+
+  return function(key, value) {
+    if(i !== 0 && typeof(censor) === 'object' && typeof(value) == 'object' && censor == value)
+      return '[Circular]';
+
+    if(i >= 29) // seems to be a harded maximum of 30 serialized objects?
+      return '[Unknown]';
+
+    ++i; // so we know we aren't using the original object anymore
+
+    return value;
+  }
+}
+
+function loadQuiz(selectedQuiz) {
+    var quiz;
+    switch (selectedQuiz) {
+        case "javascript": quiz = javascriptQuiz; break;
+        case "jdk8": quiz = jdk8Quiz; break;
+        case "jdk9": quiz = jdk9Quiz; break;
+        case "freemarker": quiz = freemarkerQuiz; break;
+        default : return {error: true, message: "Invalid quiz request", subMessage: "the requested quiz unfortunately not exists yet. Come back later!"};
+    }
+    return quiz;
+}
---
+++ pub/creative/js/quiz.js
@@ -0,0 +1,79 @@
+$(document).ready(function(){
+    var $quizSelection = $("#quizSelection"),
+        $quizBox = $("#quizBox"),
+        $networkError = $(".network-error"),
+        $modalTemplate = $("#modal-template");
+
+    $( "a.quiz-select" ).click( function( event ) {
+        var data = $(this).data('quiz');
+        event.preventDefault();
+        $.post( "/quiz-select", { data } )
+        .done( function( data ) {
+            if(data.error || !data.questions || !data.questions["1"] || !data.questions["1"].question) {
+                data.message = data.message || "invalid response";
+                data.subMessage = data.subMessage || "Unfortunately the response somehow malformed, sorry for the inconveniences";
+                invalidRequest(data.message, data.subMessage);
+                return;
+            }
+            showQuizBox(data);
+        })
+        .fail( function() {
+            $networkError.removeClass("hidden");
+            setTimeout(function(){ $networkError.addClass("hidden"); }, 2500);
+        });
+
+    });
+
+    $( ".submit" ).click( function() {
+        $.post( "/submit", { })
+        .done( function( data ) {
+            console.log("data" + JSON.stringify(data));
+        })
+        .fail( function() {
+            $networkError.removeClass("hidden");
+            setTimeout(function(){ $networkError.addClass("hidden"); }, 2500);
+        });
+    });
+
+    $( ".give-up" ).click( function() {
+
+    });
+
+    $( ".exit-anyway" ).click( function() {
+        $quizSelection.removeClass("hidden");
+        $quizBox.addClass("hidden");
+    });
+
+    function invalidRequest(message, subMessage) {
+        $modalTemplate.find(".modal-title").text(message);
+        if (subMessage) { $modalTemplate.find(".modal-body").text(subMessage); }
+        $modalTemplate.modal('show');
+    }
+
+    function showQuizBox(data) {
+        $quizBox.find(".well").removeClass("hidden");
+        $quizBox.find(".question").text(data.questions["1"].question);
+        if (data.questions["1"].a)  {
+            $quizBox.find(".answer-a").text(data.questions["1"].a);
+        } else {
+            $quizBox.find(".answer-a").parents(".well").addClass("hidden");
+        }
+        if (data.questions["1"].b)  {
+            $quizBox.find(".answer-b").text(data.questions["1"].b);
+        } else {
+            $quizBox.find(".answer-b").parents(".well").addClass("hidden");
+        }
+        if (data.questions["1"].c)  {
+            $quizBox.find(".answer-c").text(data.questions["1"].b);
+        } else {
+            $quizBox.find(".answer-c").parents(".well").addClass("hidden");
+        }
+        if (data.questions["1"].d)  {
+            $quizBox.find(".answer-d").text(data.questions["1"].b);
+        } else {
+            $quizBox.find(".answer-d").parents(".well").addClass("hidden");
+        }
+        $quizSelection.addClass("hidden");
+        $quizBox.removeClass("hidden");
+    }
+});
