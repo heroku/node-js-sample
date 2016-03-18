ad3121656d8e016378bea58bc98ca67d6207b961
Marcell Neszt <marcell_neszt@epam.com>
2016-03-18 16:55:12 +0100

    facebook dev mode implemented

--- config/auth.js
+++ config/auth.js
@@ -3,8 +3,8 @@ var devMode = false;
 module.exports = {
 
     'facebookAuth' : {
-        'clientID'      : '675122215960457',
-        'clientSecret'  : 'bcf4388b3bbf99d2f56476e462bf311d',
+        'clientID'      : devMode ? '676576985814980' : '675122215960457',
+        'clientSecret'  : devMode ? '1557913137034b22baa6fb522db345f2' : 'bcf4388b3bbf99d2f56476e462bf311d',
         'callbackURL'   : devMode ? "http://localhost:5000/auth/facebook/callback" : 'https://nodejs-quizzes.herokuapp.com/auth/facebook/callback'
     },
 
