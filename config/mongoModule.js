840286b9628b7b9f7405c42ab3bfd30191dfed9c
Marcell Neszt <marcell_neszt@epam.com>
2016-03-30 11:50:57 +0200

    mongodb finally gets working

--- config/mongoModule.js
+++ config/mongoModule.js
@@ -11,34 +11,34 @@ var connection = mongoose.connection;
 
 connection.once('open', function() {
     console.log("sucesfully connected to database");
-    setSecrets(getAppSecrets());
-    promise.fulfill();
+    //setSecrets(getAppSecrets());
+    //promise.fulfill();
 });
 
 // private functions ===========================================================
-function getAppSecrets() {
-    console.log("get app secrets");
-    readDataBasePromise.onResolve(function (err) {
-        if (err) { console.error(err); }
+//function getAppSecrets() {
+//    console.log("get app secrets");
+//    readDataBasePromise.onResolve(function (err) {
+//        if (err) { console.error(err); }
+//
+//        mongoose.model('appsecrets').find({}).exec(function(error, secretsFromDB) {
+//            if (err) { console.error(err); }
+//            readDataBasePromise.fulfill();
+//            return secretsFromDB;
+//        });
+//    });
+//}
 
-        mongoose.model('appsecrets').find({}).exec(function(error, secretsFromDB) {
-            if (err) { console.error(err); }
-            readDataBasePromise.fulfill();
-            return secretsFromDB;
-        });
-    });
-}
-
-function setSecrets(err, secretsFromDB) {
-    if (err) { console.error(err); }
-    if (secrets && secrets.length > 0) {
-        console.log("secrets exist");
-        secrets = secretsFromDB;
-    } else {
-        console.error("err, secrets does not exists. Dieeeeeee");
-        process.exit(1);
-    }
-}
+//function setSecrets(err, secretsFromDB) {
+//    if (err) { console.error(err); }
+//    if (secrets && secrets.length > 0) {
+//        console.log("secrets exist");
+//        secrets = secretsFromDB;
+//    } else {
+//        console.error("err, secrets does not exists. Dieeeeeee");
+//        process.exit(1);
+//    }
+//}
 
 // public API  =================================================================
 exports.getSecrets = function() {
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express 	= require('express');
+var async = require("async");
 var appSecrets 	= require('./app/models/appSecrets');
-var secrets = {};
-var app = express();
+var mongoose = require('mongoose');
 var bodyParser 	= require('body-parser');
 var passport 	= require('passport');
 var flash    	= require('connect-flash');
@@ -9,8 +9,10 @@ var morgan      = require('morgan');
 var cookieParser = require('cookie-parser');
 var session     = require('express-session');
 var mongooseConnection = require('./config/mongoModule');
-
 require('./config/passport')(passport);
+
+var secrets = {};
+var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.set('database_url', (process.env.DATABASE_URL || "TODOLocalDatabaseURL"));
@@ -22,18 +24,29 @@ app.use(cookieParser()); // read cookies (needed for auth)
 app.set('view engine', 'ejs'); // set up ejs for templating
 
 // required for passport
-app.use(session({ secret: mongooseConnection.getSecrets() })); // session secret
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
-app.use(flash()); // use connect-flash for flash messages stored in session
+async.series([
+    function(callback) {
+        appSecrets.findOne({}, "appSecret", function(err, obj) {
+            secrets.appSecret = obj.get("appSecret");
+            callback();
+        });
+    },
+    function(callback) {
+        app.use(session({ secret: secrets.appSecret })); // session secret
+        app.use(passport.initialize());
+        app.use(passport.session()); // persistent login sessions
+        app.use(flash()); // use connect-flash for flash messages stored in session
 
-// routes ======================================================================
-require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+        // routes ======================================================================
+        require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
-
-
-// launch ======================================================================
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+        // launch ======================================================================
+        app.listen(app.get('port'), function() {
+            console.log("Node app is running at localhost:" + app.get('port'));
+        });
+        callback();
+    }
+], function(err) { //This function gets called after the two tasks have called their "task callbacks"
+    if (err) return next(err);
+    console.log("last series");
 });
-
