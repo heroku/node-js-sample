a73f1356c4f21078b46247053c7dcdf47938d1bd
sylwolveryn <marcell_neszt@epam.com>
2016-04-01 17:28:44 +0200

    typo fix, ejs modules, admin js introduced

---
+++ public/js/admin.js
@@ -0,0 +1,13 @@
+(function() {
+    "use strict";
+    let $newQuizModal = $("#new-quiz-modal");
+
+    $(document).ready(function(){
+        $(".create-quiz").click(function() {
+            $newQuizModal.modal("show")
+        });
+    });
+
+})();
+
+
