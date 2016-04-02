59c079bd4dfff95c3a84534ce4bd04a01f828ff2
Sylwolveryn <marcell@neszt.hu>
2016-04-02 16:39:43 +0200

    create new quiz modal update

--- public/js/admin.js
+++ public/js/admin.js
@@ -6,6 +6,29 @@
         $(".create-quiz").click(function() {
             $newQuizModal.modal("show");
         });
+
+        $('[id^="collapse"]').on('hidden.bs.collapse', function () {
+            var $toggleButton = $(this).parent().find(".toggle-questions");
+            $toggleButton.html('<span class="fa fa-chevron-down"></span> Open');
+        });
+
+        $('[id^="collapse"]').on('shown.bs.collapse', function () {
+            var $toggleButton = $(this).parent().find(".toggle-questions");
+            $toggleButton.html('<span class="fa fa-chevron-up"></span> Close');
+        });
+
+        $(".save").click(function() {
+            console.log("save");
+        });
+
+        $(".discard").click(function() {
+            console.log("discard");
+        });
+
+        $(".plus-one-question").click(function() {
+            console.log("plus-one-question");
+        });
+
     });
 
 })();
