49ad8b1b00b6898ea3d478bf21573c28f549cd1c
Sylwolveryn <marcell@neszt.hu>
2016-04-11 00:04:37 +0200

    Save the result in highscore
    also code cleanup and a couple refactor

--- app/quizServer.js
+++ app/quizServer.js
@@ -1,20 +1,7 @@
-var fs = require('fs');
 var User = require('./models/user');
 var exports = module.exports = {};
-var pathToQuizzes = 'json/quizzes/';
-var HIGH_SCORE_FILE = 'json/highscore.json';
-var QUIZ_JSON = 'Quiz.json';
-var QUIZ_ANSWER_KEYS_JSON = 'QuizAnswerKeys.json';
-var quizFileNames = fs.readdirSync(pathToQuizzes);
-var quizzes = {}; updateQuizzes();
-
-exports.updateQuizzes = function() {
-    updateQuizzes();
-};
-
-exports.getQuizzes= function() {
-    return quizzes;
-};
+var Highscore = require('./models/highscore');
+var async = require('async');
 
 exports.validateAnswer = function(req, callback) {
     "use strict";
@@ -24,6 +11,7 @@ exports.validateAnswer = function(req, callback) {
         'name': req.session.quizName,
         'questionIndex': req.session.answerIndex+1
     };
+    let shouldCallback = true;
 //    console.log(JSON.stringify(req.session));
     if(!isAnswerIndexValid(req)) {
         validateAnswerResult.gameFinished = true;
@@ -31,59 +19,38 @@ exports.validateAnswer = function(req, callback) {
         validateAnswerResult.scoreUp = req.session.questionsAndAnswers[req.session.answerIndex].answers[req.body.data].point;
         req.session.score += validateAnswerResult.scoreUp;
         validateAnswerResult.gameFinished = isAnswerIndexTheLast(req.session);
-        if (validateAnswerResult.gameFinished) { /*saveHighScore(req);*/ console.log("TODO: save highscore."); }
-    }
-
-    req.session.validateAnswerResult = validateAnswerResult;
-    req.session.answerIndex++;
-    callback();
-};
-
-exports.loadQuiz = function(selectedQuiz, req) {
-    var quiz;
-    req.session.answerIndex = 0;
-
-    for ( var key in quizzes ) {
-        if (quizzes.hasOwnProperty(key) && selectedQuiz === key) {
-            try {
-                quiz = JSON.parse(fs.readFileSync(quizzes[key].quizFile, 'utf8'));
-                req.session.quizAnswers = JSON.parse(fs.readFileSync(quizzes[key].answerFile, 'utf8'));
-                req.session.quizName = quiz.name;
-                req.session.quizLength = Object.keys(quiz.questions).length;
-                req.session.score = 0;
-                break;
-            } catch(e) {
-                console.error(e);
-                return {error: true, message: "Server error.", subMessage: "the requested quiz unfortunately not available right now. Please select another one!"};
-            }
+        if (validateAnswerResult.gameFinished) {
+            saveHighScore(req, callback);
+            shouldCallback = false;
         }
     }
-    if (quiz) { return quiz; }
-    return {error: true, message: "Invalid quiz request", subMessage: "the requested quiz unfortunately not exists yet. Come back later!"};
+    req.session.validateAnswerResult = validateAnswerResult;
+    req.session.answerIndex++;
+    if ( shouldCallback ) { callback(); }
 };
-
 
 exports.showHighScoreFor = function(quizName, req) {
     "use strict";
-    var highScore,
-        responseJson = {};
-    try {
-        highScore = JSON.parse(fs.readFileSync(HIGH_SCORE_FILE, 'utf8'));
-        highScore.tables = highScore.tables || {};
-        highScore.tables.highscore_per_quiz = highScore.tables.highscore_per_quiz || {};
-        if (quizName === "all") {
-            responseJson.title = "all";
-            responseJson.body = highScore.tables.highscore_per_quiz || responseJson;
-        } else {
-            responseJson.title = quizName;
-            responseJson.body = highScore.tables.highscore_per_quiz[quizName] || responseJson;
-        }
-    } catch (e) {
-        console.error(e);
-        console.error("Exception while trying to read highscore table");
-        responseJson = {'error': true, 'message': 'Server error', 'subMessage': 'Some error happened reading the highscore. Sorry for the inconveniences'};
-    }
-    return responseJson;
+    //var highScore,
+    //    responseJson = {};
+    //try {
+    //    highScore = JSON.parse(fs.readFileSync(HIGH_SCORE_FILE, 'utf8'));
+    //    highScore.tables = highScore.tables || {};
+    //    highScore.tables.highscore_per_quiz = highScore.tables.highscore_per_quiz || {};
+    //    if (quizName === "all") {
+    //        responseJson.title = "all";
+    //        responseJson.body = highScore.tables.highscore_per_quiz || responseJson;
+    //    } else {
+    //        responseJson.title = quizName;
+    //        responseJson.body = highScore.tables.highscore_per_quiz[quizName] || responseJson;
+    //    }
+    //} catch (e) {
+    //    console.error(e);
+    //    console.error("Exception while trying to read highscore table");
+    //    responseJson = {'error': true, 'message': 'Server error', 'subMessage': 'Some error happened reading the highscore. Sorry for the inconveniences'};
+    //}
+    //return responseJson;
+    //TODO: show HighScore for quiz
 };
 
 function isAnswerIndexValid(req) {
@@ -104,54 +71,44 @@ function noAnswerWereSubmitted(req) {
 }
 
 
-function saveHighScore(req) {
+function saveHighScore(req, cb) {
     "use strict";
-    try {
-        let highScore = JSON.parse(fs.readFileSync(HIGH_SCORE_FILE, 'utf8'));
-        highScore.tables = highScore.tables || {};
-        highScore.tables.highscore_per_quiz = highScore.tables.highscore_per_quiz || {};
-        highScore.tables.highscore_per_quiz[req.session.quizName] = highScore.tables.highscore_per_quiz[req.session.quizName] || {};
-        highScore.tables.highscore_per_quiz[req.session.quizName][req.user.email] =  highScore.tables.highscore_per_quiz[req.session.quizName][req.user.email] || {};
-        highScore.tables.highscore_per_quiz[req.session.quizName][req.user.email] = updateUserScoreIfBetter(highScore.tables.highscore_per_quiz[req.session.quizName][req.user.email], req.session.score);
-
-        console.log(JSON.stringify(highScore));
-        fs.writeFileSync(HIGH_SCORE_FILE, JSON.stringify(highScore), 'utf-8');
-    } catch (e) {
-        console.error(e);
-        console.error("Exception while trying to update highscore table");
-    }
+    async.series([
+        function (callback) {
+            Highscore.findOne({'userId': req.user.id, 'quizName': req.session.quizName}, function (err, result) {
+                if (err) return callback(err);
+                if (result && result !== null) {
+                    updateUserScoreIfBetter(result, req, callback);
+                } else {
+                    saveNewScore(req, callback);
+                    callback();
+                }
+            });
+        }
+    ], function (err) {
+        cb();
+    });
 }
 
-function updateUserScoreIfBetter(oldScoreObject, newScore) {
+function updateUserScoreIfBetter(result, req, callback) {
     "use strict";
-    let newScoreObject = oldScoreObject || {};
-    console.log("newScore newScore newScore newScore newScore ");
-    console.log(JSON.stringify(newScoreObject));
-    console.log(newScore);
-    if (isObjectEmpty(newScoreObject)  || newScoreObject.score < newScore) {
-        let now = new Date((new Date() - 1000 * 60 * 60)).toISOString();
-        newScoreObject.score = newScore;
-        newScoreObject.dateTime = now.slice(0, 10) + " " + now.slice(11, 16);
+    if (result.score < req.session.score) {
+        saveNewScore(req, callback, result);
     }
-    return newScoreObject;
+    callback();
+}
+
+function saveNewScore(req, callback, result) {
+    "use strict";
+    var highscore = result || new Highscore();
+    highscore.quizName = req.session.quizName;
+    highscore.userId = req.user.id;
+    highscore.score = req.session.score;
+    highscore.save(function (err) {
+        if (err) return callback(err);
+    });
 }
 
 function isObjectEmpty(o) {
     return Object.getOwnPropertyNames(o).length === 0;
-}
-
-function updateQuizzes() {
-    "use strict";
-    quizFileNames.forEach(function(fileName) {
-        let quizNameGroups = /(.*)Quiz.json/.exec(fileName);
-        let answerKeyNameGroups = /(.*)QuizAnswerKeys.json/.exec(fileName);
-        let actualQuizName = quizNameGroups ? quizNameGroups[1] : false;
-        let actualAnswerKeyName = answerKeyNameGroups ? answerKeyNameGroups[1] : false;
-        let quizName = actualQuizName || actualAnswerKeyName;
-        quizzes[quizName] = quizzes[quizName] || {};
-        if (actualQuizName) { quizzes[quizName].quizFile = [pathToQuizzes + quizName + QUIZ_JSON].join(''); }
-        if (quizName) { quizzes[quizName].answerFile = [pathToQuizzes + quizName + QUIZ_ANSWER_KEYS_JSON].join('') }
-        quizzes[quizName].category = /(.*)_/.exec(fileName)[1];
-        quizzes[quizName].imageName = /(.*)_(.*)(Quiz.json|QuizAnswerKeys.json)/.exec(fileName)[2];
-    });
 }
