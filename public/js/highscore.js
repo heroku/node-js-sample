02bb9d391c743c18103d9c9210f0a1dcad49c0f9
sylwolveryn <marcell_neszt@epam.com>
2016-04-29 17:29:41 +0200

    Sort the highscores

--- public/js/highscore.js
+++ public/js/highscore.js
@@ -8,13 +8,14 @@
         });
 
         google.charts.load('current', {'packages': ['table']});
-        Quizzes.drawTable = function() {
-            var table = new google.visualization.Table(document.getElementById('hsBody'));
+        Quizzes.drawTable = function () {
             Quizzes.scores = Quizzes.scores || {};
-
-            var data = new google.visualization.DataTable(),
+            Quizzes.scores = sortScoresBy("quizName");
+            var table = new google.visualization.Table(document.getElementById('hsBody')),
+                data = new google.visualization.DataTable(),
                 rowLength = Quizzes.scores.user.length,
                 index;
+
 
             data.addColumn('string', 'Player');
             data.addColumn('string', 'Quiz');
@@ -43,6 +44,63 @@
                     $td.parent().addClass("highlight-gold");
                 }
             }
+        }
+
+        function sortScoresBy(property) {
+            return quickSort(Quizzes.scores, property);
+        }
+
+        function partition(items, property, left, right) {
+            var pivot   = items[property][Math.floor((right + left) / 2)],
+                i       = left,
+                j       = right;
+
+            while (i <= j) {
+                while (items[property][i] < pivot) {
+                    i++;
+                }
+                while (items[property][j] > pivot) {
+                    j--;
+                }
+                if (i <= j) {
+                    swap(items, i, j);
+                    i++;
+                    j--;
+                }
+            }
+
+            return i;
+        }
+
+        function swap(items, firstIndex, secondIndex){
+            for (var prop in items) {
+                if (hasOwnProperty.call(items, prop)) {
+                    var temp = items[prop][firstIndex];
+                    items[prop][firstIndex] = items[prop][secondIndex];
+                    items[prop][secondIndex] = temp;
+                }
+            }
+        }
+
+        function quickSort(items, property, left, right) {
+            var index;
+            if (items[property].length > 1) {
+                left = left || 0;
+                right = right || items[property].length - 1;
+
+                index = partition(items, property, left, right);
+
+                if (left < index - 1) {
+                    quickSort(items, property, left, index - 1);
+                }
+
+                if (index < right) {
+                    quickSort(items, property, index, right);
+                }
+
+            }
+
+            return items;
         }
     });
 })(window.Quizzes = window.Quizzes || {});
