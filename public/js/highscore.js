adddcedadc55e4b51f26c6366cc2f2e67a9f110a
sylwolveryn <marcell_neszt@epam.com>
2016-04-29 12:51:16 +0200

    hs beauty fix bugs and code cleanup

--- public/js/highscore.js
+++ public/js/highscore.js
@@ -1,4 +1,4 @@
-(function () {
+(function (Quizzes) {
     "use strict";
     $(document).ready(function () {
         $("body").on("click", "#highscoreModal .modal-header a", function (event) {
@@ -6,6 +6,33 @@
             $("#highscoreModal .modal-header a").parent().removeClass("active");
             $(this).parent().addClass("active");
         });
+
+        google.charts.load('current', {'packages': ['table']});
+        //google.charts.setOnLoadCallback(drawTable);
+        Quizzes.drawTable = function() {
+            var table = new google.visualization.Table(document.getElementById('hsBody'));
+            Quizzes.scores = Quizzes.scores || {};
+
+            var data = new google.visualization.DataTable(),
+                rowLength = Quizzes.scores.user.length,
+                index;
+
+            data.addColumn('string', 'Player');
+            data.addColumn('string', 'Quiz');
+            data.addColumn('string', 'Date');
+            data.addColumn('number', 'Score');
+            for (index = 0; index < rowLength; index++) {
+                data.addRows([
+                    [Quizzes.scores.user[index],
+                        Quizzes.scores.quizName[index],
+                        Quizzes.scores.date[index],
+                        Quizzes.scores.score[index]
+                    ]
+                ]);
+            }
+            table.draw(data, {showRowNumber: true, width: '100%', height: '100%'});
+        };
+
     });
 
-})();
+})(window.Quizzes = window.Quizzes || {});
