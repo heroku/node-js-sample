673f6eab193fa2c28cc22228740cbb047a0bebb0
chandevops <chandevops@gmail.com>
2016-06-07 19:10:29 -0700

    updated appName

--- deployment.js
+++ deployment.js
@@ -1,7 +1,7 @@
 #!/usr/local/bin/node
 var SimpleDeployment = require("codedeploy-scripts").SimpleDeployment;
 var deployment = new SimpleDeployment({
-    appName: "deploytest",
+    appName: "nodeappexample",
     nodePort: "5000",
     serverScript: "server.js",
     domains: "deploytest.example.com",
