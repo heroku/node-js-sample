48173d901dad9f02c1b15f7e1ad88513aa69df03
v-mneszt <v-mneszt@expedia.com>
2017-02-21 21:59:45 +0100

    start Deadpool challenges implementation
    basic routes created
    a mosaic menu idea created
    fix common js includes path

---
+++ app/challengeRoutes.js
@@ -0,0 +1,51 @@
+module.exports = function (app, passport, middlewares) {
+    "use strict";
+    // root of Deadpool challenges page. Check for authentication
+    app.get('/deadpool/*', middlewares.isLoggedIn, function (req, res) {
+    });
+
+    app.get('/deadpool/challenges', function (req, res) {
+        res.render('challenges.ejs', {
+            user: req.user
+        });
+    });
+
+    app.get('/deadpool/challenges/code/:challengeId', function (req, res) {
+        let challengeId = req.params["challengeId"];
+        res.send("/deadpool/challenges/code/" + challengeId);
+    });
+
+    app.get('/deadpool/challenges/idea/:ideaId', function (req, res) {
+        let ideaId = req.params["ideaId"];
+        res.send("get for /deadpool/challenges/code/" + ideaId);
+    });
+
+    app.post('/deadpool/challenges/idea/:ideaId', function (req, res) {
+        let ideaId = req.params["ideaId"];
+        res.send("post for /deadpool/challenges/code/" + ideaId);
+    });
+
+    app.get('/deadpool/challenges/oneliner/:oneLinerId', function (req, res) {
+        let oneLinerId = req.params["oneLinerId"];
+        res.send("get for /deadpool/challenges/code/" + oneLinerId);
+    });
+
+    app.post('/deadpool/challenges/oneliner/:oneLinerId', function (req, res) {
+        let oneLinerId = req.params["oneLinerId"];
+        res.send("post for /deadpool/challenges/code/" + oneLinerId);
+    });
+
+    app.get('/deadpool/challenges/method/:methodId', function (req, res) {
+        let methodId = req.params["methodId"];
+        res.send("get for /deadpool/challenges/code/" + methodId);
+    });
+
+    app.post('/deadpool/challenges/method/:methodId', function (req, res) {
+        let methodId = req.params["methodId"];
+        res.send("post for /deadpool/challenges/code/" + methodId);
+    });
+
+    app.get('/deadpool/history', function (req, res) {
+        res.send("/deadpool/history");
+    });
+};
---
+++ public/js/challenges.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {
+    "use strict";
+    console.log("test");
+});
