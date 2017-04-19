b80fb1811c8508552bedf1191add8dbcf6e22e05
Arastun "Russ" Efendiyev <shtooney@yahoo.com>
2017-04-19 16:46:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Russ! And Howdyyy!!!')
+  response.send('Hello Russ!')
 })
 
 app.listen(app.get('port'), function() {
