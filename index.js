8eac0b7a201055fb344884ffc0e51f941e65165b
Arastun <aefendiyev@gmail.com>
2017-04-19 19:55:25 +0000

    123

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Russ!')
+  response.send('Hello Russ! 123')
 })
 
 app.listen(app.get('port'), function() {
