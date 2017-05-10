5cbb33d5eb7887d156649a830a9a603b528363db
Arastun <aefendiyev@gmail.com>
2017-05-10 15:54:36 +0000

    hello

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('trigger myChange on Account (after insert)')
+  response.send('trigger myChange on Account (before update)')
 })
 
 app.listen(app.get('port'), function() {
