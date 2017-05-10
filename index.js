ba111065a1890e870aa9448d583404ff40aa6c07
Arastun "Russ" Efendiyev <shtooney@yahoo.com>
2017-05-10 12:04:09 -0400

    Update index.js
    heyyyyy

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('trigger myChange on Account (before update)')
+  response.send('trigger myChange on Account (after update)')
 })
 
 app.listen(app.get('port'), function() {
