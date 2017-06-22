188535876bca6b43d2cf19fcee773b9a56742a33
Arastun <aefendiyev@gmail.com>
2017-06-22 00:24:11 +0000

    mmmm

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('trigger myChange on ABC__c (after update)')
+  response.send('trigger myChange on AAAABC__c (after update)')
 })
 
 app.listen(app.get('port'), function() {
