61afd3504cb143482dce442b8c9c155f21d15812
Arastun <aefendiyev@gmail.com>
2017-06-22 00:14:53 +0000

    eeee

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('trigger myChange on Contact12 (after update)')
+  response.send('trigger myChange on Contact123 (after update)')
 })
 
 app.listen(app.get('port'), function() {
