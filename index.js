e7e31271c096256d80d65d283b66078fdf5c295f
Marc Szewczyk <marc.szewczyk@gmail.com>
2019-01-06 14:14:13 -0500

    Changed hello message.

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
+app.get('/', function (request, response) {
+  response.send('Hello World 2!')
 })
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
