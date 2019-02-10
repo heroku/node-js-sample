d35ed052287204ae42bbbcdb8a44c81924f912a7
Arash Outadi <arash.out@gmail.com>
2019-02-09 18:45:25 -0800

    Add activities endpoint

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+app.get('/activities', function(request, response){
+  const activities = [
+    {
+      timestamp: "2019-02-09",
+      duration: 10,
+      type: "run",
+    }
+  ];
+  return response.status(200).json({activities});
+});
+
 app.get('/', function(request, response) {
   response.send('Hello World!')
 })
