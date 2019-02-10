febc2365039c0764157d26cd62894a3b3a8efe76
Arash Outadi <arash.out@gmail.com>
2019-02-09 19:29:37 -0800

    Fixed post

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 var express = require('express')
+const bodyparser = require('body-parser');
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
+
 app.use(express.static(__dirname + '/public'))
+app.use(bodyparser.json())
+
+const activities = [
+  {
+    timestamp: "2019-02-09",
+    duration: 10,
+    type: "run",
+  }
+];
 
 app.get('/activities', function(request, response){
-  const activities = [
-    {
-      timestamp: "2019-02-09",
-      duration: 10,
-      type: "run",
-    }
-  ];
   return response.status(200).json({activities});
 });
+
+app.post('/activities', function(request, response){
+  const newActivities = request.body.activities;
+  for (const act of newActivities) {
+    activities.push(act);
+  }
+  return response.status(202).send();
+})
 
 app.get('/', function(request, response) {
   response.send('Hello World!')
