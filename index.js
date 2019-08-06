a02c252eaf48d36c8e329c8c01d06a3c65cf02cc
Johnathan Lyman <johnathan.lyman@heroku.com>
2019-08-06 15:06:27 -0700

    sleep

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-function waitForever() {
-  console.log("this will never happen")
+function msleep(n) {
+  Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, n);
 }
 
 app.get('/', function(request, response) {
-  setTimeout(waitForever, 31000)
+  msleep(31*1000);
   response.send('Hello Worl!d!')
 })
 
